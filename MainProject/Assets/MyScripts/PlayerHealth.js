849bb871ad55d487db476dc7f4f1316ce289af04
jirakit <songprasit.j@gmail.com>
2013-10-16 19:06:03 -0400

    add player health

---
+++ MainProject/Assets/MyScripts/PlayerHealth.js
@@ -0,0 +1,40 @@
+﻿var maximumHealth = 100.0;//maxHealth
+private var curHealth = 100.0;//currentHealth
+private var healthBarLength:float;
+
+function Start () {
+	healthBarLength = Screen.width / 2;
+}
+
+function Update () {
+	HealthBar();
+}
+function ApplyDamage (damage : float) {
+
+	if(curHealth < 0.0){
+		return;
+	}
+	
+	curHealth -= damage;
+	
+	if(curHealth <= 0.0){
+		Die();
+	}
+	
+}
+function Die () {
+
+	Destroy(this.gameObject);
+}
+function HealthBar(){
+
+	healthBarLength = (Screen.width / 2) * curHealth/ maximumHealth;
+}
+function OnGUI(){
+
+	if(curHealth < 50)
+	{
+		GUI.color = Color.red;
+	}
+	GUI.Box (new Rect(10,20,healthBarLength,20), curHealth + "/" + maximumHealth);
+}

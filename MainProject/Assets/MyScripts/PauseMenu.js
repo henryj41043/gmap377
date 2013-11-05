447e36a73d7f1336fc81d92d936fdeb47c565cb7
jirakit <songprasit.j@gmail.com>
2013-11-05 15:08:19 -0500

    Jirakit- fixed cursor issues and add toggle to the para menu

--- MainProject/Assets/MyScripts/PauseMenu.js
+++ MainProject/Assets/MyScripts/PauseMenu.js
@@ -3,7 +3,18 @@
 var mySkin : GUISkin;
 private var windowRect : Rect;
 private var paused : boolean = false;
-
+private var togglePlayer: boolean = false;
+private var toggleEnemy: boolean = false;
+private var toggleSpecialAttack1: boolean = false;
+private var toggleSpecialAttack2: boolean = false;
+private var toggleSpecialAttack3: boolean = false;
+private var toggleSpecialAttack4: boolean = false;
+private var toggleSpecialAttack5: boolean = false;
+private var toggleSpecialAttack6: boolean = false;
+private var toggleSpecialAttack7: boolean = false;
+private var toggleSpecialAttack8: boolean = false;
+private var toggleSpecialAttack9: boolean = false;
+private var toggleSpecialAttack10: boolean = false;
 var PlayerCamera : String = "10";
 var PlayerRunSpeed : String = "10";
 var PlayerBasicAttack : String = "100";
@@ -34,10 +45,14 @@ function Update () {
 	}else{
 		Time.timeScale = 1;
 	}
+	   GetComponent.<LockCursor>().enabled = true;
 }
 
 function OnGUI(){
 	if(paused){
+	
+		GetComponent.<LockCursor>().enabled = false;
+		
 		GUI.color = Color.white;
 		windowRect = GUI.Window(0, windowRect, windowFunc, "Pause Menu");
 		
@@ -70,31 +85,307 @@ function OnGUI(){
     	MeleeEnemy.damage = paraArray[6];
     	
     	paraArray[7] = System.Int32.Parse(EnemyAttackSpeed);
+    
 	}
+	
 }
 
 function windowFunc(id : int){
-	GUI.Label (Rect (50, 20, 100, 20), "CameraDistance");
-	PlayerCamera = GUI.TextField (Rect (200, 20, 100, 20), PlayerCamera, 25);
+		
+		
+
+		var GuiTextPosition = 150;
+		var GuiBoxFieldPosition = 270;
+
+		togglePlayer = GUI.Toggle(new Rect(10, 25, 140, 20), togglePlayer, "Player");
+		toggleEnemy = GUI.Toggle(new Rect(10, 45, 140, 20), toggleEnemy, "Enemy");
+		toggleSpecialAttack1 = GUI.Toggle(new Rect(10, 65, 140, 20), toggleSpecialAttack1, "Melting Pot");
+		toggleSpecialAttack2 = GUI.Toggle(new Rect(10, 85, 140, 20), toggleSpecialAttack2, "Chocolate Truffle");
+		toggleSpecialAttack3 = GUI.Toggle(new Rect(10, 105, 140, 20), toggleSpecialAttack3, "Truffle Frag");
+		toggleSpecialAttack4 = GUI.Toggle(new Rect(10, 125, 140, 20), toggleSpecialAttack4, "Sticky Slide");
+		toggleSpecialAttack5 = GUI.Toggle(new Rect(10, 145, 140, 20), toggleSpecialAttack5, "Cake Pop Polearm");
+		toggleSpecialAttack6 = GUI.Toggle(new Rect(10, 165, 140, 20), toggleSpecialAttack6, "Gummy Bind");
+		toggleSpecialAttack7 = GUI.Toggle(new Rect(10, 185, 140, 20), toggleSpecialAttack7, "Yum-yum Shield");
+		toggleSpecialAttack8 = GUI.Toggle(new Rect(10, 205, 140, 20), toggleSpecialAttack8, "Fondue Strike");
+		toggleSpecialAttack9 = GUI.Toggle(new Rect(10, 225, 140, 20), toggleSpecialAttack9, "Sweet Frost");
+		toggleSpecialAttack10 = GUI.Toggle(new Rect(10, 245, 140, 20), toggleSpecialAttack10, "Rock Candy Armor");
 	
-	GUI.Label (Rect (50, 50, 100, 20), "PlayerSpeed");
-	PlayerRunSpeed = GUI.TextField (Rect (200, 50, 100, 20),PlayerRunSpeed, 25);
+	 	if(togglePlayer)
+        {
+        		
+	        toggleEnemy = false;
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+        		
+			GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 80, 120, 20), "PlayerBasicAttack");
+			PlayerBasicAttack = GUI.TextField (Rect (GuiBoxFieldPosition, 80, 100, 20), PlayerBasicAttack, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 110, 100, 20), "PlayerHP");
+			PlayerHP = GUI.TextField (Rect (GuiBoxFieldPosition, 110, 100, 20), PlayerHP, 25);
+			
+		}
+        else if(toggleEnemy)
+        {
+        	togglePlayer = false;
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 120, 20), "EnemyRunSpeed");
+			EnemyRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), EnemyRunSpeed, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "EnemyHP");
+			EnemyHP = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20), EnemyHP, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 80, 100, 20), "EnemyDamage");
+			EnemyDamage = GUI.TextField (Rect (GuiBoxFieldPosition, 80, 100, 20), EnemyDamage, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 110, 120, 20), "EnemyAttackSpeed");
+			EnemyAttackSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 110, 100, 20), EnemyAttackSpeed, 25);
+        }
+        else if(toggleSpecialAttack1)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+				
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
 	
-	GUI.Label (Rect (50, 80, 120, 20), "PlayerBasicAttack");
-	PlayerBasicAttack = GUI.TextField (Rect (200, 80, 100, 20), PlayerBasicAttack, 25);
+        }
+        else if(toggleSpecialAttack2)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
 	
-	GUI.Label (Rect (50, 110, 100, 20), "PlayerHP");
-	PlayerHP = GUI.TextField (Rect (200, 110, 100, 20), PlayerHP, 25);
+        }
+        else if(toggleSpecialAttack3)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
 	
-	GUI.Label (Rect (50, 140, 120, 20), "EnemyRunSpeed");
-	EnemyRunSpeed = GUI.TextField (Rect (200, 140, 100, 20), EnemyRunSpeed, 25);
+        }
+        else if(toggleSpecialAttack4)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
 	
-	GUI.Label (Rect (50, 170, 100, 20), "EnemyHP");
-	EnemyHP = GUI.TextField (Rect (200, 170, 100, 20), EnemyHP, 25);
+        }
+        else if(toggleSpecialAttack5)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
 	
-	GUI.Label (Rect (50, 200, 100, 20), "EnemyDamage");
-	EnemyDamage = GUI.TextField (Rect (200, 200, 100, 20), EnemyDamage, 25);
+        }
+        else if(toggleSpecialAttack6)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
 	
-	GUI.Label (Rect (50, 230, 120, 20), "EnemyAttackSpeed");
-	EnemyAttackSpeed = GUI.TextField (Rect (200, 230, 100, 20), EnemyAttackSpeed, 25);
+        }
+        else if(toggleSpecialAttack7)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
+	
+        }
+        else if(toggleSpecialAttack8)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack9 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
+	
+        }
+        else if(toggleSpecialAttack9)
+        {
+        
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack10 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);
+        	
+	
+        }
+        else if(toggleSpecialAttack10)
+        {
+        	togglePlayer = false;
+	        toggleEnemy = false;	
+			toggleSpecialAttack1 = false;
+			toggleSpecialAttack2 = false;
+			toggleSpecialAttack3 = false;
+			toggleSpecialAttack4 = false;
+			toggleSpecialAttack5 = false;
+			toggleSpecialAttack6 = false;
+			toggleSpecialAttack7 = false;
+			toggleSpecialAttack8 = false;
+			toggleSpecialAttack9 = false;
+			
+        	GUI.Label (Rect (GuiTextPosition, 20, 100, 20), "CameraDistance");
+			PlayerCamera = GUI.TextField (Rect (GuiBoxFieldPosition, 20, 100, 20), PlayerCamera, 25);
+			
+			GUI.Label (Rect (GuiTextPosition, 50, 100, 20), "PlayerSpeed");
+			PlayerRunSpeed = GUI.TextField (Rect (GuiBoxFieldPosition, 50, 100, 20),PlayerRunSpeed, 25);	
+	
+        }
+	
+		GUI.BringWindowToFront(id); GUI.FocusWindow(id);
+		
+	
 }

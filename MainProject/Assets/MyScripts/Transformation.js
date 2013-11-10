cc5e5ec3fe3e14bbb0d348223f1da9551c155f72
Joshua Henry <henry41043@gmail.com>
2013-11-09 20:40:03 -0500

    jh - added Transformation.js

---
+++ MainProject/Assets/MyScripts/Transformation.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+private var transformEnabled = false;
+var candyBarSize = 10;
+var cooldownTime = 10;
+private var cooldown = 0;
+private var candyCount = 0;
+private var inCooldown = false;
+var candyBarGUI : GUITexture;
+var cooldownBar : GUITexture;
+private var cooldownBarWidth = 0.0;
+private var candyBarGUIWidth = 0.0;
+
+function Start () {
+	candyBarGUIWidth = candyBarGUI.pixelInset.width;
+	cooldownBarWidth = cooldownBar.pixelInset.width;
+}
+
+function Update () {
+	if(transformEnabled){
+		if(Input.GetKeyDown("1")){
+			PlayerControls.currentTransformation(1);
+			inCooldown = true;
+			transformEnabled = false;
+		}
+		if(Input.GetKeyDown("2")){
+			PlayerControls.currentTransformation(2);
+			inCooldown = true;
+			transformEnabled = false;
+		}
+		if(Input.GetKeyDown("3")){
+			PlayerControls.currentTransformation(3);
+			inCooldown = true;
+			transformEnabled = false;
+		}
+	}
+	
+	if(candyCount == candyBarSize && cooldown == 0){
+		transformEnabled = true;
+	}
+	
+	updateCandyBarGUI();
+}
+
+function addCandy(){
+	if(candyCount < candyBarSize){
+		candyCount++;
+	}
+}
+
+function resetCandyCount(){
+	candyCount = 0;
+}
+
+function updateCandyBarGUI(){
+	var candyBarFraction = Mathf.Clamp01(candyCount / candyBarSize);
+	candyBarGUI.pixelInset.xMax = candyBarGUI.pixelInset.xMin + candyBarGUIWidth * candyBarFraction;
+}
